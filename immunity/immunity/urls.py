919012144721293900554377bb122282a1450536
kells <hshi0036@student.monash.edu>
2020-08-29 23:42:17 +1000

    config static files

--- immunity/immunity/urls.py
+++ immunity/immunity/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.index),
     url(r'^index/$', views.index),
-]
+] + static(settings.STATIC_URL , document_root=settings.STATIC_ROOT)
