28b8168ae59a8b5b41ee6e39f506ef1ecd31f6b8
kells <hshi0036@student.monash.edu>
2020-08-30 21:31:59 +1000

    update database config

--- immunity/immunity/settings.py
+++ immunity/immunity/settings.py
@@ -79,19 +79,19 @@ WSGI_APPLICATION = 'immunity.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
-    'default': 
-    { 
-        'ENGINE': 'django.db.backends.mysql',    
-        'NAME': 'immunity', 
-        'HOST': '127.0.0.1', 
-        'PORT': 3306,
-        'USER': 'root',  
-        'PASSWORD': 'Aa123456', 
-    }  
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+    # 'default': 
+    # { 
+    #     'ENGINE': 'django.db.backends.mysql',    
+    #     'NAME': 'immunity', 
+    #     'HOST': '127.0.0.1', 
+    #     'PORT': 3306,
+    #     'USER': 'root',  
+    #     'PASSWORD': 'Aa123456', 
+    # }  
 }
 
 
